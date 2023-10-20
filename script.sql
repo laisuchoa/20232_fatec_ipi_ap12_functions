0a330613314789156f5339e3f65b7bcae9904cd0
Ronaldo Souza Chaves <ronaldosouzachaves@outlook.com>
2023-10-20 15:21:50 -0300

    docs(ap12): adiciona enunciados

---
+++ script.sql
@@ -0,0 +1,9 @@
+-- 1.1 Escreva a seguinte função: nome: fn_consultar_saldo; recebe: código de cliente, código de conta; devolve: o saldo da conta especificada.
+
+
+
+-- 1.2 Escreva a seguinte função: nome: fn_transferir; recebe: código de cliente remetente, código de conta remetente, código de cliente, destinatário, código de conta destinatário, valor da transferência; devolve: um booleano que indica se a transferência ocorreu ou não. Uma transferência somente pode acontecer se nenhuma conta envolvida ficar no negativo.
+
+
+
+-- 1.3 Escreva blocos anônimos para testar cada função.
